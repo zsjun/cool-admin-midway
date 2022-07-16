82db27a95fcc49349ea41105fd227fbd8f59c840
zsj <zousjun@126.com>
2022-07-16 17:42:44 +0800

    修改module/admin

--- src/modules/movie/controller/admin/category.ts
+++ src/modules/movie/controller/admin/category.ts
@@ -9,5 +9,10 @@ import { MovieCategoryEntity } from '../../entity/category';
 @CoolController({
   api: ['add', 'delete', 'update', 'info', 'list', 'page'],
   entity: MovieCategoryEntity,
+  // 分页查询配置
+  pageQueryOp: {
+    // 让title字段支持模糊查询
+    keyWordLikeFields: ['categoryName'],
+  },
 })
 export class MovieControllerAdminCatogoryController extends BaseController {}
